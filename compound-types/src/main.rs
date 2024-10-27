f47c7e55c4b17cdb35955978d5146e436713614c
Javier Alvarez <javieku@gmail.com>
2024-10-27 21:52:39 +0100

    Add compound types

---
+++ compound-types/src/main.rs
@@ -0,0 +1,95 @@
+#[derive(PartialEq, Debug)]
+// Declare Car struct to describe vehicle with four named fields
+struct Car {
+    color: String,
+    motor: Transmission,
+    roof: bool,
+    age: (Age, u32), // todo!("Move `mileage: u32` field into `age` field - a tuple with two fields: an `Age` enum, u32");
+}
+
+#[derive(PartialEq, Debug)]
+// Declare enum for Car transmission type
+enum Transmission {
+    Manual,
+    SemiAuto,
+    Automatic,
+}
+
+#[derive(PartialEq, Debug)]
+enum Age {
+    New,
+    Used,
+}
+
+// Get the car quality by testing the value of the input argument
+// - miles (u32)
+// Create a tuple for the car quality with the Age ("New" or "Used") and mileage
+// Return a tuple with the arrow `->` syntax
+fn car_quality(miles: u32) -> (Age, u32) {
+    // Declare and initialize the return tuple value
+    // For a new car, set the miles to 0
+    let quality: (Age, u32) = if (miles > 0) {
+        (Age::Used, miles)
+    } else {
+        (Age::New, miles)
+    };
+
+    // Return the completed tuple to the caller
+    return quality;
+}
+
+// Build a new "Car" using the values of four input arguments
+// - color (String)
+// - motor (Transmission enum)
+// - roof (boolean, true if the car has a hard top roof)
+// - miles (u32)
+// Call the car_quality(miles) function to get the car age
+// Return an instance of a "Car" struct with the arrow `->` syntax
+fn car_factory(color: String, motor: Transmission, roof: bool, miles: u32) -> Car {
+    // Create a new "Car" instance as requested
+    // - Bind first three fields to values of input arguments
+    // - "age" field calls "car_quality" function with "miles" input argument
+    Car {
+        color: color,
+        motor: motor,
+        roof: roof,
+        age: car_quality(miles),
+    }
+}
+
+fn main() {
+    // Create car color array
+    let colors = ["Blue", "Green", "Red", "Silver"];
+
+    // Declare the car type and initial values
+    let mut car: Car = Car {
+        color: String::from("Red"),
+        motor: Transmission::Manual,
+        roof: true,
+        age: (Age::New, 0),
+    };
+    let mut engine: Transmission = Transmission::Manual;
+
+    // Car order #1: New, Manual, Hard top
+    car = car_factory(String::from(colors[0]), Transmission::Manual, true, 0);
+    println!(
+        "Car order 1: {:?}, Hard top = {}, {:?}, {}, {} miles",
+        car.age.0, car.roof, car.motor, car.color, car.age.1
+    );
+
+    // Car order #2: Used, Semi-automatic, Convertible
+    engine = Transmission::SemiAuto;
+    car = car_factory(String::from(colors[1]), Transmission::SemiAuto, false, 100);
+    println!(
+        "Car order 2: {:?}, Hard top = {}, {:?}, {}, {} miles",
+        car.age.0, car.roof, car.motor, car.color, car.age.1
+    );
+
+    // Car order #3: Used, Automatic, Hard top
+    engine = Transmission::Automatic;
+    car = car_factory(String::from(colors[3]), Transmission::Automatic, true, 200);
+    println!(
+        "Car order 3: {:?}, Hard top = {}, {:?}, {}, {} miles",
+        car.age.0, car.roof, car.motor, car.color, car.age.1
+    );
+}
